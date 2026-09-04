3555b3af5155211780442907e61bd63353e325b4
xaavuss-coder <xaavinhooo@gmail.com>
2026-09-04 14:01:35 +0200

    losowanie liczby0

--- lotto_piatek/Program.cs
+++ lotto_piatek/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Wylosowanie Liczby");
+// wylosuj liczbe w zakrseise od 1 do 99
+
+Random random = new Random();
+
+int liczba = random.Next(1, 100);
+
+Console.WriteLine("Wyloswana liczba: " + liczba);
